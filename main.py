f02d1065d90ca7363b0dbcc0f1dda1509ab38770
azhilin <zhilin.alx@gmail.com>
2020-12-26 21:56:19 +0300

    Fix error on VideoUnavailable

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import typing
 
 import discord
 import pytube
+import pytube.exceptions
 from discord.ext import commands, tasks
 from discord_slash import SlashCommand, SlashContext
 from pytube.extract import video_id
@@ -13,6 +14,7 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 slash = SlashCommand(bot)
 
 SCHEDULED_POOL: typing.Set['ScheduledByResolutionVideo'] = set()
+MAX_RETRIES = 500
 
 
 def check_video(link: str, resolution: int) -> bool:
@@ -27,6 +29,7 @@ class ScheduledByResolutionVideo:
     author: discord.User
     link: str
     resolution: int
+    retry_count: int = 0
 
     def __hash__(self):
         return hash((self.channel, self.author, self.link, self.resolution))
@@ -47,12 +50,19 @@ async def on_ready():
 async def check_videos():
     global SCHEDULED_POOL
     sent_msgs = set()
+    discarded_msgs = set()
     for vid in SCHEDULED_POOL:
-        if check_video(link=vid.link, resolution=vid.resolution):
-            sent_msgs.add(vid)
-            await vid.channel.send(f"{vid.author.mention}: {vid.link}")
+        try:
+            if check_video(link=vid.link, resolution=vid.resolution):
+                sent_msgs.add(vid)
+                await vid.channel.send(f"{vid.author.mention}: {vid.link}")
+        except pytube.exceptions.VideoUnavailable:
+            vid.retry_count += 1
+            if vid.retry_count > MAX_RETRIES:
+                discarded_msgs.add(vid)
 
     SCHEDULED_POOL -= sent_msgs
+    SCHEDULED_POOL -= discarded_msgs
 
 
 @slash.subcommand(base="youtube", subcommand_group="schedule", name="resolution")
@@ -63,17 +73,26 @@ async def schedule_resolution(ctx: SlashContext, link: str, resolution: int = 10
     except pytube.extract.RegexMatchError:
         await ctx.send(content=f"This is not a valid YouTube video link!", complete_hidden=True)
         return
+    except pytube.exceptions.VideoUnavailable:
+        msg_content = (
+            f"Came into problem trying to retrieve the video (might be too new). "
+            f"Anyways, will post it as soon as it will become available at {resolution}!"
+        )
+        watch_url = link
+    else:
+        msg_content = (
+            f"Came into problem trying to retrieve the video (might be too new). "
+            f"Anyways, will post it as soon as it will become available at {resolution}!"
+        )
+        watch_url = video.watch_url
 
     SCHEDULED_POOL.add(ScheduledByResolutionVideo(
         channel=ctx.channel,
         author=ctx.author,
-        link=video.watch_url,
+        link=watch_url,
         resolution=resolution,
     ))
-    await ctx.send(
-        content=f'Alrighty. Will send "{video.title}" here as soon as its quality reaches {resolution}p!',
-        complete_hidden=True,
-    )
+    await ctx.send(content=msg_content, complete_hidden=True,)
 
 
 if __name__ == '__main__':
